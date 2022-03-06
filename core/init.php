ba0a470ac76de28ed16d057d0439cf0ebe90aca6
Jurica101 <kuzmic.jurica@gmail.com>
2022-03-06 21:39:14 +0100

    Deletion of everything

--- core/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Autoloader za automatsko učitavanje svih klasa (4.ZAD)
-session_start();
-
-spl_autoload_register(function ($class) {
-    $parts = explode("\\", $class); // razbija putanju do klase pomoću \\ kao separatora
-    include_once "./". end($parts) . ".php"; // uzima zadnji dio iz gornjeg explode-a i ubacuje ga u putanju gdje se nalaze klase a to je u folderu classes
-});
--- modules/login.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once "../core/init_modules.php";
-
-$authentication = new AuthSystem($_POST["username"], $_POST["password"]);
